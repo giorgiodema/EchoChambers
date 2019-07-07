90b7de5095ec49651b801559ff6a9205ecde7ccc
giorgiodema <giorgiodema@gmail.com>
2019-07-07 12:33:40 +0200

    scores

--- src/domains_extractor/load_domains.py
+++ src/domains_extractor/load_domains.py
@@ -3,9 +3,14 @@ import re
 
 PATH = os.path.join("fake_news_dataset","fake.csv")
 
-# Return a set of domains extracted from kaggle dataset
+# Return the set of domains extracted by kaggle and a dictionary with
+# the score of each domain
 def load():
         with open(PATH, 'r', encoding='utf-8') as f:
                 data = f.read()
-                urls = set(re.findall('http[s]?://.[^/]*/',data))
-        return urls
+                urls = re.findall('http[s]?://.[^/]*/',data)
+                urls_set = set(urls)
+                scores = {}
+                for url in urls_set:
+                        scores[url] = urls.count(url)
+        return urls_set,scores
