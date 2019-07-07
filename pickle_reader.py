bf95ddae6aaf9600f9d8286d3e1649262958aedf
giorgiodema <giorgiodema@gmail.com>
2019-07-07 17:28:40 +0200

    cleanup

--- pickle_reader.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-from pprint import pprint
-
-data = pickle.load(open('domains_dump-3-LEVELS.pickle', 'rb'))
-
-pprint(data)
---
+++ src/domains_processing/processing.py
@@ -0,0 +1,7 @@
+from src.domains_extractor.load_domains import load
+
+domains,scores = load()
+
+def koggle_domains_proximity(d):
+    for dk in domains:
+        pass
