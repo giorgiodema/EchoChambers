8a4856ff8bb2758c2ba487b7f22fb2c7a2ee3e91
Sergio Pedri <sergio0694@live.com>
2019-07-07 16:59:15 +0200

    Added empty Word2VecPreprocessor project

---
+++ src/twitter_dotnet/Word2VecPreprocessor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Word2VecPreprocessor
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
