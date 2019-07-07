aec9d4c42a22b108b3b1e8f237a14abac82b3cfa
giorgiodema <giorgiodema@gmail.com>
2019-07-07 11:38:54 +0200

    refactor

--- src/domains-extractor/load_domains.py
+++ src/domains-extractor/load_domains.py
@@ -4,7 +4,7 @@ import re
 PATH = os.path.join("fake-news","fake.csv")
 
 # Return a set of domains extracted from kaggle dataset
-def load_dataset():
+def load():
         with open(PATH, 'r', encoding='utf-8') as f:
                 data = f.read()
                 urls = set(re.findall('http[s]?://.[^/]*/',data))
