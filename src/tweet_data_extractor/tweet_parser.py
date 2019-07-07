ad62c571df799216fa0f008798a703e3ace219ad
Spadala <pietro.spa@gmail.com>
2019-07-07 17:58:10 +0200

    Added domains extract and bug fixes

--- src/tweet_data_extractor/tweet_parser.py
+++ src/tweet_data_extractor/tweet_parser.py
@@ -6,6 +6,16 @@ from collections import defaultdict
 import os
 
 
+def _extract_domain(link: str) -> str:
+	"""Extract the domain of an url"""
+	link_split = link.split('/')
+	if link.startswith('http'):
+		domain = link_split[2] if len(link_split)>=3 else None
+	else:
+		domain = None
+	return domain
+
+
 def get_user_data():
     JSON_PATH = "tweets_json"  #json directory path
     CACHE_FILE = "user_data_cached.pickle"
@@ -17,7 +27,7 @@ def get_user_data():
 
     print(" [*] Creating user data from tweets...")
 
-    users_data = defaultdict(lambda: (list(), list()))  #users_data[user] = (domains list, retweet list)
+    users_data = {}    #users_data[user] = (domains list, retweet list)
 
     for filename in tqdm(os.listdir(JSON_PATH)):
         if filename.endswith(".json"): 
@@ -25,16 +35,32 @@ def get_user_data():
                 tweets = json.load(json_file)
                 
                 for tweet in tweets:
-                    # urls
                     urls = tweet["entities"]["urls"]
                     user = tweet["user"]["id"]
-                    for url in urls:
-                        users_data[user][0].append(url["expanded_url"])
 
-                    # retweets
+                    # create user key in users_data
+                    if not user in users_data:
+                    	users_data[user] = (list(), list())
+
+                    # retrieve urls
+                    for url in urls:
+                    	url = url["expanded_url"]
+                    	# if the url is internal in twitter return it all
+                    	if "twitter" in url[:20]:
+                        	users_data[user][0].append(url)
+                        # else if the url is not internal in twitter then take only its domain
+                        else:
+                        	domain = _extract_domain(url)
+                        	if domain is None:
+                        		continue
+                        	else:
+                        		users_data[user][0].append(domain)
+                        		
+
+                    # retrieve retweets
                     if "retweeted_status" in tweet and not tweet["retweeted_status"] is None:
-                        user_id = tweet["retweeted_status"]["user"]["id"]
-                        users_data[user][1].append(user_id)
+                        user_rtw = tweet["retweeted_status"]["user"]["id"]
+                        users_data[user][1].append(user_rtw)
 
     result = {}
     for user, data in users_data.items():
@@ -45,5 +71,5 @@ def get_user_data():
     return result
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 	get_user_data()
