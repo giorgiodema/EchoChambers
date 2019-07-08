800cf57ff8e149143ea062f02cbd817afe183229
Spadala <pietro.spa@gmail.com>
2019-07-08 09:11:29 +0200

    Now data extractor returns directly user graph

--- src/tweet_data_extractor/tweet_parser.py
+++ src/tweet_data_extractor/tweet_parser.py
@@ -1,5 +1,7 @@
 import json
 import pickle
+import networkx as nx
+import itertools
 from pprint import pprint
 from tqdm import tqdm
 from collections import defaultdict
@@ -16,9 +18,10 @@ def _extract_domain(link: str) -> str:
 	return domain
 
 
-def get_user_data():
-    JSON_PATH = "tweets_json"  #json directory path
-    CACHE_FILE = "user_data_cached.pickle"
+def get_user_graph():
+    BASE_PATH = os.path.join("src", "tweet_data_extractor")
+    JSON_PATH = os.path.join(BASE_PATH, "tweets_json")  #json directory path
+    CACHE_FILE = os.path.join(BASE_PATH, "user_data.pickle")
 
     if os.path.isfile(CACHE_FILE):
         print(" [*] Pickled data found, loading...")
@@ -27,49 +30,59 @@ def get_user_data():
 
     print(" [*] Creating user data from tweets...")
 
-    users_data = {}    #users_data[user] = (domains list, retweet list)
+    G = nx.Graph()
+    users, domains = set(), set()
 
+    # Create bipartite graph   < users - domains > 
     for filename in tqdm(os.listdir(JSON_PATH)):
         if filename.endswith(".json"): 
-            with open(os.path.join(JSON_PATH, filename)) as json_file:
+            with open(os.path.join(JSON_PATH, filename), "r", encoding="utf-8") as json_file:
                 tweets = json.load(json_file)
                 
                 for tweet in tweets:
                     urls = tweet["entities"]["urls"]
                     user = tweet["user"]["id"]
 
-                    # create user key in users_data
-                    if not user in users_data:
-                    	users_data[user] = (list(), list())
+                    # create user key in users
+                    if not user in users:
+                        users.add(user)
+                        G.add_node(user)
 
-                    # retrieve urls
+                    # retrieve urls domains
                     for url in urls:
                         url = url["expanded_url"]
+
                         # if the url is internal in twitter return it all
                         if "twitter" in url[:20]:
-                        	users_data[user][0].append(url)
+                        	domain = url
                         # else if the url is not internal in twitter then take only its domain
                         else:
                             domain = _extract_domain(url)
                             if domain is None:
                                 continue
-                            else:
-                                users_data[user][0].append(domain)
-                        		
 
-                    # retrieve retweets
-                    if "retweeted_status" in tweet and not tweet["retweeted_status"] is None:
-                        user_rtw = tweet["retweeted_status"]["user"]["id"]
-                        users_data[user][1].append(user_rtw)
+                        # if domain first appereance, create key in domains
+                        if not domain in domains:
+                            domains.add(domain)
+                        
+                        # if edge does not exist create it with weigth 1
+                        if not G.has_edge(user, domain):
+                            G.add_edge(user, domain, weigth=1)
+                        else:
+                            G[user][domain]["weigth"] += 1
 
-    result = {}
-    for user, data in users_data.items():
-        result[user] = {"domains":data[0], "retweets":data[1]}
+    # Merge bipartite graph: for each pair of users that points the same domain add an edge between them
+    G_result = nx.Graph()
+    G_result.add_nodes_from(users)
+    for domain in tqdm(domains):
+        linked_usrs = G[domain]
+        for user1, user2 in itertools.combinations(linked_usrs, 2):
+            G_result.add_edge(user1, user2)
 
-    pickle.dump(result, open(CACHE_FILE, "wb"))
+    pickle.dump(G_result, open(CACHE_FILE, "wb"))
 
-    return result
+    return G_result
 
 
 if __name__ == "__main__":
-	get_user_data()
+	get_user_graph()
