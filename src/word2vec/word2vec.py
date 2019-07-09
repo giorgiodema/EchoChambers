d287fa3acf72c8e169bbcc16adbe9e006f5f8e85
Sergio Pedri <sergio0694@live.com>
2019-07-09 11:48:32 +0200

    Added basic w2v network

--- src/word2vec/word2vec.py
+++ src/word2vec/word2vec.py
@@ -1,5 +1,6 @@
 from os import path
-from data_preprocessing import load_dictionary, load_dataset
+import tensorflow as tf
+from data_preprocessing import load_dictionary, load_dataset, generate_batches
 
 # constants
 
@@ -20,6 +21,47 @@ TMP_DIR = "/tmp/"
 
 # dataset loading
 dictionary, direct_map, inverse_map = load_dictionary(DICTIONARY_PATH)
-print('>> {} loaded words'.format(len(dictionary)))
+DICTIONARY_SIZE = len(dictionary)   # size of the loaded dictionary
+print('>> {} loaded words'.format(DICTIONARY_SIZE))
 dataset = load_dataset(DATASET_PATH, SAMPLE_SIZE)
 print('>> {} loaded sentences'.format(len(dataset)))
+
+# model definition
+with tf.Graph().as_default():
+
+    # input tensors
+    with tf.name_scope('inputs'):
+        train_inputs = tf.placeholder(tf.int32, shape=[BATCH_SIZE])
+        train_labels = tf.placeholder(tf.int32, shape=[BATCH_SIZE, NUM_TRUE]) # outputs for every input word
+
+    # variable tensors
+    with tf_name_scope('variables'):
+        word_embeddings = tf.Variable(tf.random_uniform([DICTIONARY_SIZE, EMBEDDING_SIZE], -1.0, 1.0), name='word_embeddings')
+        context_embeddings = tf.Variable(tf.truncated_normal([DICTIONARY_SIZE, EMBEDDING_SIZE], stddev=1.0 / math.sqrt(EMBEDDING_SIZE)), name='context_embeddings') # swapped shapes for sampled softmax
+        output_biases = tf.Variable(tf.zeros([DICTIONARY_SIZE])) # needed by the sampled softmax
+    lookup = tf.nn.embedding_lookup(word_embeddings, train_inputs) # forward inputs to the hidden layer
+
+    # loss
+    with tf.name_scope('loss'):
+        softmax = tf.nn.sampled_softmax_loss(
+            weights=context_embeddings,
+            biases=output_biases,
+            labels=train_labels,
+            inputs=lookup,
+            num_classes=DICTIONARY_SIZE,
+            num_sampled=NEG_SAMPLES,
+            num_true=NUM_TRUE
+        )
+        loss = tf.reduce_sum(softmax, name='reduce_sum')
+
+    # track the batch loss in the summary
+    tf.summary.scalar('loss', loss)
+
+    # optimizer
+    with tf.name_scope('optimizer'):
+        optimizer = tf.train.AdamOptimizer().minimize(loss)
+
+    # additional objects
+    merged = tf.summary.merge_all() # merged summaries
+    init = tf.global_variables_initializer() # variables initializer
+    saver = tf.train.Saver() # network saver
