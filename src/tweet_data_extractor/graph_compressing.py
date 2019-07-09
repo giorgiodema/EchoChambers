e52e639a5ca0a59a0bcbe88c56ff4a8c6c55e5b6
Spadala <pietro.spa@gmail.com>
2019-07-09 14:27:41 +0200

    Added code to merge dict-compressed graphs

--- src/tweet_data_extractor/graph_compressing.py
+++ src/tweet_data_extractor/graph_compressing.py
@@ -12,6 +12,14 @@ with open(GRAPH_FILE, "rb") as f:
     G = pickle.load(f)
 
 
+'''G = {      #test code
+    1: Counter({2:1, 3:1}),
+    2: Counter({4:1, 1:1, 3:1}),
+    3: Counter({1:1, 4:1, 2:1}),
+    4: Counter({3:1, 2:1})
+}'''
+
+
 
 def prune_nodes():
     """Discards nodes that have less than [1,5] edges"""
@@ -90,6 +98,29 @@ def compress_graph_weighted_dict():
         pickle.dump(graph, f)
 
 
+
+def merge_graphs_weighted_dict():
+    G1 = G
+    with open(os.path.join("raw", "graph-compressed_weighted_set_1.pickle"), "rb") as f:
+        G2 = pickle.load(f)
+    '''G2 = {           #test code
+        2: Counter({3:1, 5:1}),
+        3:Counter({2:1, 5:1}),
+        5:Counter({2:1, 3:1})
+    }'''
+
+    for n in set(G1.keys()).union(set(G2.keys())):
+        if (n in G1) and (n in G2):
+            G1[n] = G1[n] + G2[n]
+        elif (not n in G1) and (n in G2):
+            G1[n] = G2[n].copy()
+        if n in G2:
+            del G2[n]
+    
+    with open(os.path.join("raw", "graph-compressed_weighted_set_merged.pickle"), "wb") as f:
+        pickle.dump(G1, f)
+
+
 def compress_graph_weighted_list():
     graph = {}
     for node in tqdm(list(G.nodes)):
@@ -105,3 +136,4 @@ def compress_graph_weighted_list():
 
 if __name__ == '__main__':
     compress_graph_weighted_dict()
+    #merge_graphs_weighted_dict()
